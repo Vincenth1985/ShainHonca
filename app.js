e5f446f0b864b0b8bb4fdf40ab388e1b4fcc3b8b
Vincent Honca <vincenth1985@gmail.com>
2019-12-04 18:11:10 +0100

    working on the styling and smoothscroll

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ t1.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     { x: "0%", ease: Power2.easeInOut },
     "-=1.2"
   );
+
+var Pictures = [];
+var x = 3;
+
+Pictures.push("/img/2.jpg");
+Pictures.push("/img/5.jpg");
+Pictures.push("/img/3.jpg");
+
+function imgGallery() {
+  Pictures.forEach(element => {
+    console.log(element);
+  });
+}
+
+$(".navbar a").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html,body").animate({ scrollTop: $(hash).offset().top }, 800);
+  }
+});
+
+function sendmail() {
+  var name = document.getElementById("name");
+  var email = document.getElementById("email");
+  var message = document.getElementById("message");
+}
